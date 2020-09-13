d063e6f3bd997507039c55e1815a358fdb77497f
Nick <CoffeeBeforeArch@gmail.com>
2020-09-13 13:09:39 -0500

    add spinlock with backoff example

---
+++ spinlock/opt2.cpp
@@ -0,0 +1,78 @@
+// This program benchmarks an improved spinlock C++
+// Optimizations:
+//  1.) Spin locally (decreases traffic)
+//  2.) Backoff
+// By: Nick from CoffeeBeforeArch
+
+#include <benchmark/benchmark.h>
+
+#include <atomic>
+#include <cstdint>
+#include <thread>
+
+// Simple Spinlock
+// Lock now performs local spinning
+struct Spinlock {
+  // Lock is just an atomic bool
+  std::atomic<bool> locked{false};
+
+  // Locking mechanism
+  void lock() {
+    // Keep trying
+    while (1) {
+      // Try and grab the lock
+      // Return if we get the lock
+      if (!locked.exchange(true)) return;
+
+      // If we didn't get the lock, just read the value which gets cached
+      // locally. This leads to less traffic.
+      // Designed to improve the performance of spin-wait loops.
+      while (locked.load()) __builtin_ia32_pause();
+    }
+  }
+
+  // Unlocking mechanism
+  // Just set the lock to free (0)
+  // Can also use the assignment operator
+  void unlock() { locked.store(false); }
+};
+
+// Increment
+void inc(Spinlock &s, std::int64_t &val) {
+  for (int i = 0; i < 100000; i++) {
+    s.lock();
+    val++;
+    s.unlock();
+  }
+}
+
+// Benchmark for naive spinlock
+static void naive_spinlock(benchmark::State &s) {
+  // Sweep over a range of threads
+  auto num_threads = s.range(0);
+
+  // Value we will increment
+  std::int64_t val = 0;
+
+  // Allocate a vector of threads
+  std::vector<std::thread> threads;
+  threads.reserve(num_threads);
+
+  Spinlock sl;
+
+  // Timing loop
+  for (auto _ : s) {
+    for (auto i = 0u; i < num_threads; i++) {
+      threads.emplace_back([&] { inc(sl, val); });
+    }
+    // Join threads
+    for (auto &thread : threads) thread.join();
+    threads.clear();
+  }
+}
+BENCHMARK(naive_spinlock)
+    ->DenseRange(1, std::thread::hardware_concurrency())
+    ->UseRealTime()
+    ->Unit(benchmark::kMillisecond);
+
+BENCHMARK_MAIN();
